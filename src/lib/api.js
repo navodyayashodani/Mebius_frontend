338d98246156a8c02723f08f17ba55ee4ce3abaa
Harindi Navodya <141401570+navodyayashodani@users.noreply.github.com>
2025-02-22 10:17:06 +0530

    commit

--- src/lib/api.js
+++ src/lib/api.js
@@ -6,7 +6,7 @@ export const Api = createApi({
   reducerPath: "Api",
   tagTypes: ["Products", "Categories"],
   baseQuery: fetchBaseQuery({ 
-    baseUrl: "fed-storefront-backend-harindi.vercel.app/api",
+    baseUrl: "https://fed-storefront-backend-harindi.vercel.app/api",
     prepareHeaders: async (headers) => {
       try {
         const token = await window.Clerk.session?.getToken();
